aee9d8d42a32aaec35ab960cec88e37cf5d11a90
Hanna Negash <hnegash.n@gmail.com>
2023-11-23 12:09:40 -0800

    Work with GET, POST, PATCH, and DELETE requests

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,18 +1,40 @@
 import React, { useState } from "react";
 
-function ItemForm() {
+//(post6) Deconstruct the onAddItem
+function ItemForm({ onAddItem }) {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+  // (post1) Add function to handle submissions - using the data from the form state with right key/value pairs
+  function handleSubmit(e) {
+    e.preventDefault();
+    const itemData = {
+      name: name,
+      category: category,
+      isInCart: false,
+    }; // (post2) fetch
+    fetch("http://localhost:4000/items", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(itemData),
+    })
+      .then(r => r.json())
+      //Use this prop in the ItemForm to send the new item up to the ShoppingList when we receive a response from the server
+      .then(newItem => onAddItem(newItem)); //(post7) call the onAddItem prop with the newItem
+  }
+
   return (
-    <form className="NewItem">
+    // Set up the form to call handleSubmit when the form is submitted
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
         <input
           type="text"
           name="name"
           value={name}
-          onChange={(e) => setName(e.target.value)}
+          onChange={e => setName(e.target.value)}
         />
       </label>
 
@@ -21,8 +43,7 @@ function ItemForm() {
         <select
           name="category"
           value={category}
-          onChange={(e) => setCategory(e.target.value)}
-        >
+          onChange={e => setCategory(e.target.value)}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -7,11 +7,44 @@ function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
 
+  //Update state by passing the array of items to setItems - Display items
+  useEffect(() => {
+    fetch("http://localhost:4000/items")
+      .then(r => r.json())
+      .then(items => setItems(items));
+  }, []);
+
+  //(post3) Add this function! to pass a callback function as a prop for ItemForm file (ShoppingList is the parent component of ItemForm)
+  //Add a handleAddItem function to ShoppingList, and pass a reference to that function as a prop called onAddItem to the ItemForm:
+  function handleAddItem(newItem) {
+    setItems([...items, newItem]); //(post4)setState with a new array with our current items from state, plus the new item at the end
+  }
+
+  //(patch2) Add this callback function to pass it as a prop to the Item component
+  //Call setState by creating a new array which contains the updated item in place of the old item. Use .map create this new array
+  function handleUpdateItem(updatedItem) {
+    const updatedItems = items.map(item => {
+      if (item.id === updatedItem.id) {
+        return updatedItem;
+      } else {
+        return item;
+      }
+    });
+    setItems(updatedItems);
+  }
+
+  //(delete2) Add this callback function to pass it as a prop to the Item component
+  //Call setState with a new array that removes the deleted item from the list. Use .filter to help create this new array:
+  function handleDeleteItem(deletedItem) {
+    const updatedItems = items.filter(item => item.id !== deletedItem.id);
+    setItems(updatedItems);
+  }
+
   function handleCategoryChange(category) {
     setSelectedCategory(category);
   }
 
-  const itemsToDisplay = items.filter((item) => {
+  const itemsToDisplay = items.filter(item => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
@@ -19,14 +52,22 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      {/* (post5) add the onAddItem prop! */}
+      <ItemForm onAddItem={handleAddItem} />
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+        {/* (patch3) pass it as a prop to Item */}
+        {/* (delete3) pass it as a prop to Item */}
+        {itemsToDisplay.map(item => (
+          <Item
+            key={item.id}
+            item={item}
+            onUpdateItem={handleUpdateItem}
+            onDeleteItem={handleDeleteItem}
+          />
         ))}
       </ul>
     </div>
